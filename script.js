3f2cdd3cede855661602cd1382d3c433a54df4ea
thejackforge <kimfidler@gmail.com>
2020-12-07 15:09:29 -0700

    New Layout - New Code

--- script.js
+++ script.js
@@ -48,15 +48,6 @@ function projectsList() {
 }
 
 
-// function populateDomProjects(index) {
-//     main.innerHTML = '';
-//     const operatorDiv = document.createElement('div')
-//     operatorDiv.className = "mb-20"
-//     const operatorData = projectsArray[index].data
-//     operatorDiv.innerHTML = operatorData;
-//     main.appendChild(operatorDiv)
-// }
-
 // Event Listeners for Navigation
 
 showModalBtn.addEventListener('click', () => {
@@ -72,14 +63,12 @@ modalCloseBtn.addEventListener('click', () => {
 
 window.addEventListener('click', (e) => {
     const infoModalShow = document.getElementById('info-modal-show');
-    const modalContent = document.getElementById('modal-content');
     if (infoModalShow.style.display === 'flex') {
         if (e.target === infoModalShow) {
             infoModalShow.style.display = 'none';
         }
     }
 })
-
 
 projectsBtn.addEventListener('click', projectsOpen)
 
@@ -94,97 +83,6 @@ projectsUl.addEventListener('click', (e) => {
 
 // PROJECTS SECTION
 
-// Progress Bar - Initiate Hack Project
+// How to Build a Progress Bar Project
 
-function progressInitialHack() {
-    console.log('CONNECTION MADE')
-    const progressInitialHackComplete = document.getElementById('progress-initial-hack-complete')
-    
-    let height = 0;
-    let hack = setInterval(progress, 20);
-        function progress() {
-            progressInitialHackComplete.className = 'progress-initial-hack-complete'
-            if (height >= 100) {
-                clearInterval(hack);
-            } else {
-                height++;
-                progressInitialHackComplete.style.height = height + '%';
-                    if (height === 100) {
-                        console.log('SENDING DATA')
-                        progressConnectHack();
-                }
-            }
-        }     
-}
-
-function progressConnectHack() {
-    const progressConnectHackComplete = document.getElementById('progress-connect-hack-complete')
-    let width = 0;
-    const hack = setInterval(progress, 40);
-        function progress() {
-            progressConnectHackComplete.className = 'progress-connect-hack-complete'
-            if (width >= 100) {
-                clearInterval(hack);
-            } else {
-                width++;
-                progressConnectHackComplete.style.width = width + '%';
-                    if (width === 100) {
-                        console.log('HACK PREPARED')
-                        progressHackComplete();
-                    }
-            }
-        }
-}
-
-function progressHackComplete() {
-    const progressHackButton = document.getElementById('progress-hack-button');
-    progressHackButton.style.color = 'lime'
-    progressHackButton.addEventListener('click', () => {
-        const progressHack = document.getElementById('progress-hack-complete')
-        let width = 0;
-        const hack = setInterval(progress, 50);
-            function progress() {
-                if (width >= 100) {
-                    clearInterval(hack);
-                } else {
-                    width++;
-                    progressHack.style.width = width + '%'
-                    progressHack.innerText = `${width}%`
-                        if (width === 100) {
-                            progressHack.innerText = `HACK COMPLETE`
-                            console.log('HACK COMPLETE')
-                        }
-                }
-            }
-    })
-}
-
-function progressRookieHack() {
-    console.log('Rookie Hack Initiated')
-    const rookieHack = document.getElementById('rookie-progress-hack');
-        let width = 0;
-        const hack = setInterval(progress, 30)
-            function progress() {
-                if (width >= 100) {
-                clearInterval(hack);
-                    } else {
-                        width++;
-                        rookieHack.style.width = width + '%';
-                    } if (width === 100) {
-                        rookieHack.innerText = 'OK'
-                        console.log('Rookie Hack Complete // Rating: Sloppy')
-                    }
-             }  
-}
-
-main.addEventListener('click', (e) => {
-    if (e.target.id === 'progress-button') {
-        progressRookieHack();
-    }
-})
-
-main.addEventListener('click', (e) => {
-    if (e.target.id === 'progress-initial-button') {
-        progressInitialHack();
-    }
-})
+// How to Build a Modal Project
